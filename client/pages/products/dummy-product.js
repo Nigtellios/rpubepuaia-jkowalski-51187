b4dd23076aad8d84a606920f0a1024fe4164d6b8
Jakub Kowalski <contact@jakubkowalski.dev>
2022-12-27 10:08:36 +0100

    feat: added footer, header, various header/footer components

--- client/pages/products/dummy-product.js
+++ client/pages/products/dummy-product.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function DummyProduct() {
   return (
     <div>
       <h1>Dummy Product</h1>
+      <h2>
+        <Link href="/">Home</Link>
+      </h2>
     </div>
   );
 }
