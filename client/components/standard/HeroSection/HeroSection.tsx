5d83ca50c2f8b78bec152d104b5a5b61c4e5c06f
Jakub Kowalski <contact@jakubkowalski.dev>
2023-01-08 21:42:50 +0100

    feat: hero section in strapi

---
+++ client/components/standard/HeroSection/HeroSection.tsx
@@ -0,0 +1,10 @@
+import styles from './HeroSection.module.scss';
+
+// Here we'll use https://splidejs.com/integration/react-splide/
+
+export default function HeroSection() {
+  return (
+    <section className={ styles.hero }>
+    </section>
+  );
+}
